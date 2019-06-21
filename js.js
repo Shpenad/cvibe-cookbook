de313319f250bce80746f577add34b94f89f8a6c
Nenad Nikoloski <nikoloski.mail@gmail.com>
2019-06-21 20:59:04 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
-﻿            var glagol, objekat, likovi;
-            glagol = ["sadim", "pečem", "berem", "čistim", "plevim", "kuvam", "farbam", "perem", "gledam", "kupim", "natočim", "sipam", "iseckam", "čitam", "pevam", "kujem", "štemujem", "avanzujem", "liferujem", "slušam", "štimungujem", "naštimam", "cepam", "jedem", "usisam", "premazem", "namazem", "peglam", "krečim", "bacim", "oplevim", "hejtujem", "bušim", "operem", "ribam", "skinem", "zapakujem", "spustim", "zakačim", "pomazim", "naštelujem", "evidentiram", "šarmiram", "ziherujem", "kibicujem", "isfeniram", "brifingujem", "adaptiram", "aktiviram", "degustiram", "gepekujem", "dizajniram", "švercujem", "prodam", "gletujem"];
-            objekat = ["repu", "travu", "cigle", "sudove", "ogradu", "kelerabu", "televizor", "auto", "cveće", "kera", "drva", "ajvar", "amortizer", "kuplung", "čakšire", "duvan", "bubreg", "emigrante", "farmu", "kantar", "vaser vagu", "krompir", "kravu", "pidzamu", "lampu", "knjigu", "ulje", "krov", "mistriju", "noge", "dupe", "uši", ""];
-            likovi = ["mamom i tatom", "mamom", "tatom", "babom", "Dart Vejderom", "Aleksandrom Vucicem", "Perom", "Djokom", "Mitom", "kolegom"];
-            preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+            var glagol, objekat, likovi;
+            glagol = ["sadim", "pečem", "berem", "čistim", "plevim", "kuvam", "farbam", "gledam", "kupim", "natočim", "sipam", "iseckam", "čitam", "pevam", "kujem", "štemujem", "avanzujem", "liferujem", "slušam", "štimungujem", "naštimam", "cepam", "jedem", "usisam", "premazem", "namazem", "peglam", "krečim", "bacim", "oplevim", "bušim", "operem", "ribam", "skinem", "zapakujem", "zakačim", "pomazim", "naštelujem", "šarmiram", "ziherujem", "kibicujem", "isfeniram", "gepekujem", "dizajniram", "švercujem", "prodam", "gletujem"];
+            objekat = ["repu", "travu", "cigle", "sudove", "ogradu", "kelerabu", "televizor", "auto", "cveće", "kera", "drva", "ajvar", "kuplung", "čakšire", "duvan", "emigrante", "farmu", "kantar", "vaser vagu", "krompir", "kravu", "pidzamu", "lampu", "knjigu", "ulje", "krov", "mistriju", "noge", "dupe", "kokoške", "petla", "stepenice"];
+            likovi = ["mamom i tatom", "mamom", "tatom", "babom", "Dart Vejderom", "Aleksandrom Vučićem", "bratom", "Djokom", "Ozrenom", "kolegom", "Bokijem", "Binom", "Korovom", "Divnom Ljubojević", "Evom Ras", "komšijom", "komšinicom"];
 
             function randGen() {
               return Math.floor(Math.random() * 5);
@@ -16,6 +15,6 @@
               //                var i = randGen();
               var content = "Ne mogu moram da " + glagol[rand1] + " " + objekat[rand2] + " sa " + likovi[rand3] + ".";
 
-              document.getElementById('sentence').innerHTML= "&quot;" + content + "&quot;";
+              document.getElementById('sentence').innerHTML = "&quot;" + content + "&quot;";
             };
             sentence();
