c6e565bba5a2ed4d9e9542d6391a6cb8e0476598
Nenad Nikoloski <nikoloski.mail@gmail.com>
2019-06-22 16:00:30 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,26 +1,35 @@
-            var glagol, objekat, likovi, arhiva;
+            var glagol, objekat, likovi, arhiva, deprapridev, depraglagol, predlog, deprapridev2, depraobjekat, depraglagol2, depralikovi ;
             
-            glagol = ["sadim", "pečem", "berem", "čistim", "plevim", "kuvam", "farbam", "gledam", "kupim", "natočim", "sipam", "iseckam", "čitam", "pevam", 
-            "kujem", "štemujem", "avanzujem", "liferujem", "slušam", "štimungujem", "naštimam", "cepam", "jedem", "usisam", "premazem", "namazem", "peglam", 
-            "krečim", "bacim", "oplevim", "bušim", "operem", "ribam", "skinem", "zapakujem", "zakačim", "pomazim", "naštelujem", "šarmiram", "ziherujem", 
-            "kibicujem", "isfeniram", "gepekujem", "dizajniram", "švercujem", "prodam", "gletujem"];
+            glagol = ["sadim", "pecem", "berem", "cistim", "plevim", "kuvam", "farbam", "gledam", "kupim", "natocim", "sipam", "iseckam", "citam", "pevam", 
+            "kujem", "stemujem", "avanzujem", "liferujem", "slusam", "stimungujem", "nastimam", "cepam", "jedem", "usisam", "premazem", "namazem", "peglam", 
+            "krecim", "bacim", "oplevim", "busim", "operem", "ribam", "skinem", "zapakujem", "zakacim", "pomazim", "nastelujem", "sarmiram", "ziherujem", 
+            "kibicujem", "isfeniram", "gepekujem", "dizajniram", "svercujem", "prodam", "gletujem"];
              
-            objekat = ["repu", "travu", "cigle", "sudove", "ogradu", "kelerabu", "televizor", "auto", "cveće", "kera", "drva", "ajvar", "kuplung", 
-            "čakšire", "duvan", "emigrante", "farmu", "kantar", "vaser vagu", "krompir", "kravu", "pidzamu", "lampu", "knjigu", "ulje", "krov", 
-            "mistriju", "noge", "dupe", "kokoške", "petla", "stepenice"];
+            objekat = ["repu", "travu", "cigle", "sudove", "ogradu", "kelerabu", "televizor", "auto", "cvece", "kera", "drva", "ajvar", "kuplung", 
+            "caksire", "duvan", "emigrante", "farmu", "kantar", "vaser vagu", "krompir", "kravu", "pidzamu", "lampu", "knjigu", "ulje", "krov", 
+            "mistriju", "noge", "dupe", "kokoske", "petla", "stepenice"];
             
             
-            likovi = ["mamom i tatom", "mamom", "tatom", "babom", "Dart Vejderom", "Aleksandrom Vučićem", "bratom", "Djokom", "Ozrenom", "kolegom",
-            "Bokijem", "Binom", "Korovom", "Divnom Ljubojević", "Evom Ras", "komšijom", "komšinicom"];
+            likovi = ["mamom i tatom", "mamom", "tatom", "babom", "Dart Vejderom", "Aleksandrom Vucicem", "bratom", "Djokom", "Ozrenom", "kolegom",
+            "Bokijem", "Binom", "Korovom", "Divnom Ljubojevic", "Evom Ras", "komsijom", "komsinicom"];
             
             arhiva = ["Ne mogu moram sutra sa mamom i tatom na planinarenje.", "Ne mogu moram da idem sa babom na bus.", "Ne mogu stize mi kamion sutra.", 
             "Ne mogu slabo sam spavao zbog Ozrenovog petla.", "Ne mogu moram sutra na basket sa bratom.", "Ne mogu radije bih istrco.",
-            "Tanka prica.", "Ne mogu nosim cigle sutra", "Ne mogu, moram da idem, kasno je.", "Necu u grad bucno je", "Ne mogu moram sa bratom da gledam seriju",
-             "Ne mogu moram sa komsinicom da cepam drva", "Ne mogu da dolazim do grada moram da jedem kuvano", "Ne mogu na trening mokre su mi noge",
-             "Ne mogu moram da farbam ogradu", "Ne mogu moram da radim stepenice sutra", "Ne mogu moram da usisam", "Ne mogu na fudbal dolazi mi elektricar", 
-             "Ne mogu na koncert kasno pocinje", "Ne mogu moram da cistim sprat", "Ne mogu moram da nosim snimke ct-a u kamenicu", "Ne mogu moram da zalijem cvece",
-             "Ne mogu moram da setam kera", "Ne mogu moram da gledam plocice", "Ne mogu moram sa Korovom da gledam odela", "Ne mogu moram da idem na Divnu Ljubojevic",
-             "Ne mogu moram da premazem terasu firnajzom", "Ne mogu na ng moram da setam  kera sa bratom", "Ne mogu verio mi se drugar", "Ne mogu moram da pecem jagnje "];
+            "Tanka prica.", "Ne mogu nosim cigle sutra.", "Ne mogu, moram da idem, kasno je.", "Necu u grad bucno je.", "Ne mogu moram sa bratom da gledam seriju.",
+             "Ne mogu moram sa komsinicom da cepam drva.", "Ne mogu da dolazim do grada moram da jedem kuvano.", "Ne mogu na trening mokre su mi noge.",
+             "Ne mogu moram da farbam ogradu.", "Ne mogu moram da radim stepenice sutra.", "Ne mogu moram da usisam.", "Ne mogu na fudbal dolazi mi elektricar.", 
+             "Ne mogu na koncert kasno pocinje.", "Ne mogu moram da cistim sprat.", "Ne mogu moram da nosim snimke ct-a u kamenicu.", "Ne mogu moram da zalijem cvece.",
+             "Ne mogu moram da setam kera.", "Ne mogu moram da gledam plocice.", "Ne mogu moram sa Korovom da gledam odela.", "Ne mogu moram da idem na Divnu Ljubojevic.",
+             "Ne mogu moram da premazem terasu firnajzom.", "Ne mogu na Novu Godinu moram da setam  kera sa bratom", "Ne mogu verio mi se drugar.", "Ne mogu moram da pecem jagnje."  ]
+             
+             
+             deprapridev = ["besciljno", "razocarano", "dubokoumno", "filozofski", "esencijalno", "egzistencijalno", "moralno", "pesimisticno"]
+             depraglagol = ["buljim", "tonem", "propadam", "mracim", "postojim", "raspravim", "nestanem" ]
+             predlog = ["u", "na", "oko", "iz"]
+             deprapridev2 = ["beskonacnu", "besciljnu", "emotivnu", "beznadeznu", "depresivnu"]
+             depraobjekat = ["entropiju", "depresiju", "rupu", "jamu", "planinu", "prepreku"]
+             depraglagol2 = ["postojanja", "entropije", "egzistencije", "egzistencijalizma", "bespuca"]
+             depralikovi = ["Kamijem", "Kafkom", "Niceom", "de Sadom", "Sartrom", "Dostojevskim"]
 
             function randGen() {
               return Math.floor(Math.random() * 5);
@@ -31,17 +40,31 @@
               var rand2 = Math.floor(Math.random() * objekat.length);
               var rand3 = Math.floor(Math.random() * likovi.length);
               var rand4 = Math.floor(Math.random() * arhiva.length);
-              var rand5 = Math.floor(Math.random() * 2);
+              var rand5 = Math.floor(Math.random() * 3);
+              var rand6 = Math.floor(Math.random() * deprapridev.length);
+              var rand7 = Math.floor(Math.random() * depraglagol.length);
+              var rand8 = Math.floor(Math.random() * predlog.length);
+              var rand9 = Math.floor(Math.random() * deprapridev2.length);
+              var rand10 = Math.floor(Math.random() * depraobjekat.length);
+              var rand11 = Math.floor(Math.random() * depraglagol2.length);
+              var rand12 = Math.floor(Math.random() * depralikovi.length);
               //                var randCol = [rand1,rand2,rand3];
               //                var i = randGen();
               var content = "Ne mogu moram da " + glagol[rand1] + " " + objekat[rand2] + " sa " + likovi[rand3] + ".";
-              var arhivski = arhiva[rand4];
+              var arhivski = arhiva[rand4]
+              var depra = "Ne mogu moram da " + deprapridev[rand6] + " " + depraglagol[rand7] + " " 
+              + predlog[rand8] + " " + deprapridev2[rand9] + " " + depraobjekat[rand10] + " " + depraglagol2[rand11] " sa " + depralikovi[rand12] + ".";
               
-				if (rand5 === 0) {
-                document.getElementById('sentence').innerHTML = "&quot;" + content  + "&quot;";
-                }
-                else {
-                document.getElementById('sentence').innerHTML = "&quot;" + arhivski  + "&quot;";
-                }
-            }
+              
+							if (rand5 == 0) {
+              								document.getElementById('sentence').innerHTML = "&quot;" + content  + "&quot;";
+            									}
+            	else if (	rand5 === 1){
+            									document.getElementById('sentence').innerHTML = "&quot;" + depra  + "&quot;";
+            		}							
+            									
+            	else {
+              			document.getElementById('sentence').innerHTML = "&quot;" + arhivski  + "&quot;";
+            				}
+            };
             sentence();
